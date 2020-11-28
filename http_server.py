0ca26176ed51b9cfae5dbcbd84f6075fd2aacb8e
ZachCusick <21976390+ZachCusick@users.noreply.github.com>
2020-11-27 21:15:57 -0500

    Added post functionality and logging

--- http_server.py
+++ http_server.py
@@ -9,17 +9,36 @@ import socketserver
 import threading
 import http.server
 
-file = open("project-honey/HomePage.html", "r")
+
 class ThreadedHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        file = open("project-honey/HomePage.html", "r")
         self.wfile.write(bytes(file.read(), "utf-8"))
         address = self.client_address
         data = "GET request version: " + self.request_version
         log(address, data)
+    
+    def do_HEAD(self):
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+
+    def do_POST(self):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      content_length = int(self.headers['Content-Length'])
+      post_data = self.rfile.read(content_length)
+      post_data = "Data: " + post_data.decode("utf-8")
+      data = "POST request version: " + self.request_version + "\nUser-Agent: " + self.headers['User-Agent'] + "\n"
+      file = open("project-honey/HomePage.html", "r")
+      self.wfile.write(bytes(file.read(), "utf-8"))
+      address = self.client_address
+      log(address, data + post_data)
                       
 
 class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
@@ -27,8 +46,8 @@ class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
 
 def log(address, data):
   sep = '-' * 50
-  with open('./log.txt', 'a') as file:
-    file.write('Time: {}\nIP: {}\nPort: {}\nData: {}\n'.format(time.ctime(), address[0], address[1], data))
+  with open('./HTTPlog.txt', 'a') as file:
+    file.write('Time: {}\nIP: {}\nPort: {}\nRequest: {}\n'.format(time.ctime(), address[0], address[1], data))
     file.write(sep + '\n')
 
 
@@ -38,8 +57,12 @@ def run_pot():
   host = ''
   port = 80
 
+  #server = http.server.HTTPServer((host, port), ThreadedHTTPRequestHandler)
+
   server = ThreadedHTTPServer((host, port), ThreadedHTTPRequestHandler)
   ip, port = server.server_address
+  
+  #server.serve_forever()
 
   # Start a thread with the server -- that thread will then start one
   # more thread for each request
